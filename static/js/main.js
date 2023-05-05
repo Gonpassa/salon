330c3e8bc92a2f39b142af58f4f6f6d744b80a2d
Gonpassa <Gonzalopassa@hotmail.com>
2023-05-05 18:53:10 -0400

    media queries

--- static/js/main.js
+++ static/js/main.js
@@ -8,3 +8,16 @@ function setActive(){
     document.querySelector('.active').classList.remove('active')
     this.classList.add('active')    
 }
+
+const hamburgerMenu = document.querySelector('.fa-bars')
+
+hamburgerMenu.addEventListener('click', showMenu)
+
+function showMenu(){
+    const nav = document.querySelector('.nav')
+    if(nav.style.display === 'none'){
+        nav.style.display = 'block'
+    }else{
+        nav.style.display = 'none'
+    }
+}
